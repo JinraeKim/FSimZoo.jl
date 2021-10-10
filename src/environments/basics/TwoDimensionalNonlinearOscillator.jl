72156f1d1853d8df930ac0fb189f6100a7dd997e
JinraeKim <kjl950403@gmail.com>
2021-10-10 16:48:32 +0900

    Bug fix

--- src/environments/basics/TwoDimensionalNonlinearOscillator.jl
+++ src/environments/basics/TwoDimensionalNonlinearOscillator.jl
@@ -2,8 +2,12 @@
 A two dimensional nonlinear oscillator introduced in [1].
 Note that the system is constructed by converse HJB method [2].
 # References
-[1] 
-[2]
+[1] J. A. Primbs, “Nonlinear Optimal Control: A Receding Horizon Approach,”
+California Institute of Technology, Pasadena, California, 1999.
+[2] J. Doyle, J. A. Primbs, B. Shapiro, and V. Nevistic,
+“Nonlinear Games: Examples and Counterexamples,”
+in Proceedings of 35th IEEE Conference on Decision and Control, Kobe, Japan, 1996,
+vol. 4, pp. 3915–3920. doi: 10.1109/CDC.1996.577292.
 """
 struct TwoDimensionalNonlinearOscillator <: AbstractEnv
 end
@@ -24,7 +28,7 @@ function Dynamics!(env::TwoDimensionalNonlinearOscillator)
                  -x1 * (π/2 + atan(5*x1))
                  - (5*x1^2)/(2*(1+25*x1^2))
                  + 4*x2
-                 + 3*u
+                 + 3*u[1]
                 )
     end
 end
@@ -32,12 +36,12 @@ end
 function RunningCost(env::TwoDimensionalNonlinearOscillator)
     return function (x, u)
         @unpack x1, x2 = x
-        r = x2^2 + u^2
+        r = x2^2 + u[1]^2
     end
 end
 
 function OptimalValue(env::TwoDimensionalNonlinearOscillator)
-    return function (x, u)
+    return function (x)
         @unpack x1, x2 = x
         V_star = x1^2 * (π/2 + atan(5*x1)) + x2^2
     end
