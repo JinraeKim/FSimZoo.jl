cfbdb06ba1a484591959d1c3fdcc2cb536b6e90f
JinraeKim <kjl950403@gmail.com>
2021-10-27 14:20:31 +0900

    Fix the structure of TwoDimensionalNonlinearDTSystem

--- src/environments/basics/TwoDimensionalNonlinearDTSystem.jl
+++ src/environments/basics/TwoDimensionalNonlinearDTSystem.jl
@@ -4,7 +4,7 @@ A two dimensional nonlinear discrete time (DT) system.
 
 Base.@kwdef struct TwoDimensionalNonlinearDTSystem <: AbstractEnv
     c = 0.75
-    d = 1e-1
+    d = 10.0
 end
 
 function State(env::TwoDimensionalNonlinearDTSystem)
@@ -14,18 +14,18 @@ function State(env::TwoDimensionalNonlinearDTSystem)
 end
 
 function Dynamics!(env::TwoDimensionalNonlinearDTSystem)
-    @unpack c = env
+    @unpack c, d = env
     @Loggable function dynamics!(dx, x, p, t; u)
         @unpack x1, x2 = x
         @log state = x
         @log input = u
-        dx.x1 = c * (x1 + x2)  # x1_next = dx.x1
-        dx.x2 = c * x2 + u[1]  # x2_next = dx.x2
+        dx.x1 = c*(x1 + x2)  # x1_next = dx.x1
+        dx.x2 = c*x2 + u[1]  # x2_next = dx.x2
     end
 end
 
 function CubicSolution(env::TwoDimensionalNonlinearDTSystem)
-    @unpack c = env
+    @unpack c, d = env
     return function (x2)
         a = (
              (cbrt(sqrt(3)*sqrt(27*c^2*x2^2 + 8) + 9*c*x2) / 3^(2/3))
@@ -35,30 +35,30 @@ function CubicSolution(env::TwoDimensionalNonlinearDTSystem)
 end
 
 function RunningCost(env::TwoDimensionalNonlinearDTSystem)
-    @unpack c = env
+    @unpack c, d = env
     return function (x, u)
-        @unpack x2 = x
+        @unpack x1, x2 = x
         a = CubicSolution(env)(x2)
         r = (
-             (3/4)*a^4 + a^2 + x'*[(1-c^2) -c^2; -c^2 (1-2*c^2)]*x
-             + (1/4)*u[1]^4
-            )
+             (3/4)*a^4 + a^2
+             - d*sqrt(abs(c*x1+c*x2)) + d*sqrt(abs(x1))
+            )  # for V(x) = d*sqrt(abs(x1)) + x2^2
     end
 end
 
 function OptimalValue(env::TwoDimensionalNonlinearDTSystem)
-    @unpack d = env
+    @unpack c, d = env
     return function (x)
         @unpack x1, x2 = x
-        d*(x1^2 + x2^2)
+        d*sqrt(abs(x1)) + x2^2
     end
 end
 
 function OptimalQValue(env::TwoDimensionalNonlinearDTSystem)
     return function (x, u)
         dx = copy(x)
-        Dynamics!(env)(dx, x, nothing, 0.0; u=u)
-        OptimalValue(env)(dx) + RunningCost(env)(x, u)
+        Dynamics!(env)(dx, x, nothing, nothing; u=u)
+        OptimalValue(env)(dx) + RunningCost(env)(x, u)  # V(x_next) + r(x, u)
     end
 end
 
