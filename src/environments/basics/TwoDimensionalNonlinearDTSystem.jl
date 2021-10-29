edbbe4d71bca85a74c8f2e211514ad3509471dd1
JinraeKim <kjl950403@gmail.com>
2021-10-29 23:54:12 +0900

    Fix the value of TwoDimensionalNonlinearDTSystem

--- src/environments/basics/TwoDimensionalNonlinearDTSystem.jl
+++ src/environments/basics/TwoDimensionalNonlinearDTSystem.jl
@@ -3,13 +3,24 @@ A two dimensional nonlinear discrete time (DT) system.
 """
 
 Base.@kwdef struct TwoDimensionalNonlinearDTSystem <: AbstractEnv
-    c = 0.75
-    d = 10.0
+    c = 0.5
+    d = 3.0
 end
 
 function State(env::TwoDimensionalNonlinearDTSystem)
     return function (x1, x2)
         ComponentArray(x1=x1, x2=x2)
+    end
+end
+
+function V1(env::TwoDimensionalNonlinearDTSystem)
+    return function (x)
+        if x > 0
+            ans = x^(1/4)
+        else
+            ans = x^4
+        end
+        ans
     end
 end
 
@@ -21,6 +32,7 @@ function Dynamics!(env::TwoDimensionalNonlinearDTSystem)
         @log input = u
         dx.x1 = c*(x1 + x2)  # x1_next = dx.x1
         dx.x2 = c*x2 + u[1]  # x2_next = dx.x2
+        @onlylog next_state = dx
     end
 end
 
@@ -38,11 +50,15 @@ function RunningCost(env::TwoDimensionalNonlinearDTSystem)
     @unpack c, d = env
     return function (x, u)
         @unpack x1, x2 = x
+        dx = copy(x)  # will be next state
+        Dynamics!(env)(dx, x, nothing, nothing; u=u)
         a = CubicSolution(env)(x2)
-        r = (
+        r̄ = (
              (3/4)*a^4 + a^2
-             - d*sqrt(abs(c*x1+c*x2)) + d*sqrt(abs(x1))
+             - d*V1(env)(dx.x1) + d*V1(env)(x1)
+             + (1-c^2)*x2^2
             )  # for V(x) = d*sqrt(abs(x1)) + x2^2
+        r = r̄ + (1/4)*u[1]^4
     end
 end
 
@@ -50,7 +66,7 @@ function OptimalValue(env::TwoDimensionalNonlinearDTSystem)
     @unpack c, d = env
     return function (x)
         @unpack x1, x2 = x
-        d*sqrt(abs(x1)) + x2^2
+        d*V1(env)(x1) + x2^2
     end
 end
 
