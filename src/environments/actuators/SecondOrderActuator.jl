e188bd184d11d3f6275fa61ef71f2ecae55b6ebc
JinraeKim <kjl950403@gmail.com>
2023-05-15 17:44:09 +0900

    Add logging

--- src/environments/actuators/SecondOrderActuator.jl
+++ src/environments/actuators/SecondOrderActuator.jl
@@ -13,8 +13,10 @@ end
 
 function Dynamics!(env::SecondOrderActuator)
     (; ζ, ω) = env
-    return function dynamics!(dx, x, p, t; δ_cmd)
+    @Loggable function dynamics!(dx, x, p, t; δ_cmd)
         (; δ, δ̇) = x
+        @log state = x
+        @log input = δ_cmd
         dx.δ = δ̇
         dx.δ̇ = -2*ζ*ω*δ̇ + ω^2*(δ_cmd-δ)
     end
